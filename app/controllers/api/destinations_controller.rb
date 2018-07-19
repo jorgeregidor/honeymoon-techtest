132f07507d9a541bbda109ee4974d7a16635ce9a
Jorge Regidor <jorgeregidor@gmail.com>
2018-07-19 23:24:13 +0100

    Task 2

--- app/controllers/api/destinations_controller.rb
+++ app/controllers/api/destinations_controller.rb
@@ -1,8 +1,22 @@
 class Api::DestinationsController < ApplicationController
+  
+	before_action :get_destination, only:[:show]
+
   # GET /api/destinations
   # GET /api/destinations/json
   def index
     @destinations = Destination.all
     render json: @destinations.map(&:to_h)
   end
+
+  def show
+  	render json: @destination
+  end
+
+  private 
+
+  def get_destination
+  	@destination = Destination.find(params[:id])
+  end
+
 end
