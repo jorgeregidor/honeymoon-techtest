d95f95c7aaebbc8edb8828ca45d91a19b76336cb
Jorge Regidor <jorgeregidor@gmail.com>
2018-07-19 23:21:23 +0100

    Task 1

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "should save user" do
-    user = User.new
+    user = User.new(name: "Jorge")
     assert user.save
   end
 end
